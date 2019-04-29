596a61e2f11434cb7e03d704bfc1a2dcb2fd5657
Valonga, Fabio <fabiosv.etep@gmail.com>
2019-04-28 23:53:23 -0300

    api fix controllers import + burgers rest methods

--- api/v1.0/burgers.js
+++ api/v1.0/burgers.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
-const burgers_controller = require('../../controllers/burgers_controller')
+const burgers_controller = require('../../controllers/v1.0/burgers_controller')
 
 router.get('/', burgers_controller.getAll)
-// router.post('/', burgers_controller.create)
-// router.put('/', burgers_controller.update)
-// router.delete('/', burgers_controller.delete)
+router.post('/', burgers_controller.create)
+router.put('/', burgers_controller.update)
+router.delete('/', burgers_controller.delete)
 
 module.exports = router
