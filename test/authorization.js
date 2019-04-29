0919e15e577c4df841e1e23a866f5d838d0cadc5
Valonga, Fabio <fabiosv.etep@gmail.com>
2019-04-29 00:43:02 -0300

    authorization test

---
+++ test/authorization.js
@@ -0,0 +1,74 @@
+const server = require('../server')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const should = chai.should()
+const validatePost = require('../helpers/validation_methods').validatePost
+const validatePut = require('../helpers/validation_methods').validatePut
+
+chai.use(chaiHttp)
+
+const authorize_msg = "Please provide an Authorization header to identify yourself"
+
+describe('API v1.0 Authorization', function(){
+  it('/GET-POST-PUT-DELETE/burgers : verify if burgers API is under protection, should display 403 - "Please provide an Authorization header to identify yourself"', function(done){
+    chai.request(server)
+      .get('/api/v1.0/burgers')
+      .end((err, res) => {
+        res.should.have.status(403)
+        res.body.should.be.a('object')
+        res.body['error'].should.equal(authorize_msg)
+      })
+
+    let burger = {name: "X-Test", ingredients: []}
+
+    chai.request(server)
+      .post('/api/v1.0/burgers')
+      .send(burger)
+      .end((err, res) => {
+        res.should.have.status(403)
+        res.body.should.be.a('object')
+        res.body['error'].should.equal(authorize_msg)
+      })
+
+    chai.request(server)
+      .put('/api/v1.0/burgers')
+      .send(burger)
+      .end((err, res) => {
+        res.should.have.status(403)
+        res.body.should.be.a('object')
+        res.body['error'].should.equal(authorize_msg)
+      })
+
+    done()
+  })
+
+  it('/GET/ingredients : verify if ingredients API is under protection, should display 403 - "Please provide an Authorization header to identify yourself"', function(done){
+    chai.request(server)
+    .get('/api/v1.0/ingredients')
+    .end((err, res) => {
+      res.should.have.status(403)
+      res.body.should.be.a('object')
+      res.body['error'].should.equal(authorize_msg)
+    })
+
+    chai.request(server)
+      .post('/api/v1.0/ingredients')
+      .send({name: "Tomate", price: 0.10})
+      .end((err, res) => {
+        res.should.have.status(403)
+        res.body.should.be.a('object')
+        res.body['error'].should.equal(authorize_msg)
+      })
+
+    chai.request(server)
+      .put('/api/v1.0/ingredients')
+      .send({name: "Alface", price: 0.10})
+      .end((err, res) => {
+        res.should.have.status(403)
+        res.body.should.be.a('object')
+        res.body['error'].should.equal(authorize_msg)
+      })
+
+    done()
+  })
+})
